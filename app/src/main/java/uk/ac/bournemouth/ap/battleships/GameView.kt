f120d51d3e6ffba852bfb06a69cbbf657c5154b2
David Whalley <77077742+dwhalley15@users.noreply.github.com>
2023-03-18 14:53:55 +0000

    Made changes to GameView to include headers for both x and x axis..

--- app/src/main/java/uk/ac/bournemouth/ap/battleships/GameView.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/battleships/GameView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 
@@ -16,18 +17,32 @@ class GameView: View {
         defStyleAttr
     )
 
-    private val colCount get() = 10
-    private val rowCount get() = 10
+    private val colCount get() = 11
+    private val rowCount get() = 11
 
     private val backPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = Color.rgb(0,157,196)
     }
 
+    private val headerPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.WHITE
+    }
+
     private val linePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL_AND_STROKE
         color = Color.BLACK
     }
+
+    private val wordPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        setTextAlign(Paint.Align.CENTER)
+        setTextSize(20f * resources.displayMetrics.density)
+        setTypeface(Typeface.SANS_SERIF)
+        color = Color.BLACK
+    }
+
+
 
     override fun onDraw(canvas: Canvas) {
 
@@ -36,19 +51,43 @@ class GameView: View {
         val canvasHeight = height.toFloat()
 
         //Set the cell size
-        val cellWidth = canvasWidth / colCount
-        val cellHeight = canvasHeight / rowCount
+        val cellWidth = minOf(canvasWidth / colCount)
+        val cellHeight = minOf(canvasHeight / rowCount)
 
         //Draw background
         canvas.drawRect(0f,0f, canvasWidth, canvasHeight, backPaint)
 
-        //Draw grid column lines
+        //Draw column headers
+        for(x in 0..colCount){
+            canvas.drawRect(x*cellWidth,0f,cellWidth, cellHeight,headerPaint)
+        }
+
+        //Draw column numbers
+        var xCount = 0
         for(x in 1..colCount){
+            canvas.drawText("$xCount", x*cellWidth+cellWidth/2,cellHeight/2,wordPaint)
+            xCount += 1
+        }
+
+        //Draw row headers
+        for(y in 0..rowCount){
+            canvas.drawRect(0f,y*cellHeight,cellWidth, cellHeight,headerPaint)
+        }
+
+        //Draw row numbers
+        var yCount = 0
+        for(y in 1..rowCount){
+            canvas.drawText("$yCount", cellWidth/2,y*cellHeight+cellHeight/2,wordPaint)
+            yCount += 1
+        }
+
+        //Draw grid column lines
+        for(x in 0..colCount){
             canvas.drawLine(x * cellWidth, 0f, x * cellWidth, canvasHeight, linePaint)
         }
 
         //Draw grid row lines
-        for(y in 1..rowCount){
+        for(y in 0..rowCount){
             canvas.drawLine(0f, y * cellHeight, canvasWidth, y * cellHeight, linePaint)
         }
 
